6f039f885528ff08cc981ede5563052a5e5025ed
Chad Smith <chad@brightkeycloud.com>
2019-11-07 15:24:43 -0700

    fixed typos and updated terraform templates to 0.12.13 format

--- 02_GlobalVPCNetwork/terraform/eu-central-1/main.tf
+++ 02_GlobalVPCNetwork/terraform/eu-central-1/main.tf
@@ -6,8 +6,5 @@ resource aws_cloudformation_stack "demo-vpc" {
   name = "eu-central-1"
   template_body = "${file("./eu-central-1/demo-vpc-eu-central-1.yaml")}"
   provider = "aws.region"
-  tags {
-    CostCenter = "demo"
-  }
 }
 
--- 02_GlobalVPCNetwork/terraform/us-east-2/main.tf
+++ 02_GlobalVPCNetwork/terraform/us-east-2/main.tf
@@ -6,8 +6,5 @@ resource aws_cloudformation_stack "demo-vpc" {
   name = "us-east-2"
   template_body = "${file("./us-east-2/demo-vpc-us-east-2.yaml")}"
   provider = "aws.region"
-  tags {
-    CostCenter = "demo"
-  }
 }
 
