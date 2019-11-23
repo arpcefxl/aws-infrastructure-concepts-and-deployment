40f1a97d3090d8c1b1ac8bf2a2b849d047537e13
Chad Smith <csmith@MacBook-Pro-2.local>
2019-11-22 19:53:09 -0700

    added python to 10

--- 10_S3SameRegionReplication/create_s3_structure.sh
+++ 10_S3SameRegionReplication/create_s3_structure.sh
@@ -6,4 +6,15 @@ BUCKET1=
 BUCKET2=
 REGION=
 
-aws s3 --profile PROFILE mb 
+#Create the two buckets
+aws s3api --profile $PROFILE create-bucket --bucket $BUCKET1 --region $REGION
+aws s3api --profile $PROFILE create-bucket --bucket $BUCKET2 --region $REGION
+
+#Populate the first bucket
+aws s3 --profile $PROFILE cp bucket1/ s3://$BUCKET1 --recursive
+
+#Enable bucket versioning on both buckets
+aws s3api --profile $PROFILE put-bucket-versioning --bucket $BUCKET2 --versioning-configuration --region $REGION
+aws s3api --profile $PROFILE put-bucket-versioning --bucket $BUCKET1 --versioning-configuration --region $REGION
+
+
