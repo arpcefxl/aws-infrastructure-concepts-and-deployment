757363ad8f2e6c26ebb3fc08b4f83a011f3be216
Chad Smith <csmith@MacBook-Pro-2.local>
2019-12-09 20:53:12 -0700

    updated 10

--- 10_S3SameRegionReplication/create_s3_structure.sh
+++ 10_S3SameRegionReplication/create_s3_structure.sh
@@ -17,5 +17,8 @@ aws s3 --profile $PROFILE cp bucket1/ s3://$BUCKET1 --recursive
 aws s3api --profile $PROFILE put-bucket-versioning --bucket $BUCKET2 --versioning-configuration Status=Enabled --region $REGION
 aws s3api --profile $PROFILE put-bucket-versioning --bucket $BUCKET1 --versioning-configuration Status=Enabled --region $REGION
 
-aws s3 --profile $PROFILE cp --recursive s3://$BUCKET1 s3://$BUCKET2
+#Enable bucket replication in the AWS console
+#After this, run the following command to sync existing objects
+#from the source to destination bucket:
+#aws s3 --profile $PROFILE cp --recursive s3://$BUCKET1 s3://$BUCKET2
 
