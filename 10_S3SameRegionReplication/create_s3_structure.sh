dafbb9ce4982ef39717408b9ae8d975d3fe3b18a
Chad Smith <csmith@MacBook-Pro-2.local>
2019-12-09 20:52:35 -0700

    updated 10

--- 10_S3SameRegionReplication/create_s3_structure.sh
+++ 10_S3SameRegionReplication/create_s3_structure.sh
@@ -14,7 +14,8 @@ aws s3api --profile $PROFILE create-bucket --bucket $BUCKET2 --region $REGION
 aws s3 --profile $PROFILE cp bucket1/ s3://$BUCKET1 --recursive
 
 #Enable bucket versioning on both buckets
-aws s3api --profile $PROFILE put-bucket-versioning --bucket $BUCKET2 --versioning-configuration --region $REGION
-aws s3api --profile $PROFILE put-bucket-versioning --bucket $BUCKET1 --versioning-configuration --region $REGION
+aws s3api --profile $PROFILE put-bucket-versioning --bucket $BUCKET2 --versioning-configuration Status=Enabled --region $REGION
+aws s3api --profile $PROFILE put-bucket-versioning --bucket $BUCKET1 --versioning-configuration Status=Enabled --region $REGION
 
+aws s3 --profile $PROFILE cp --recursive s3://$BUCKET1 s3://$BUCKET2
 
