d9cb5747766023481d91db67c4f563901f246438
Chad Smith <csmith@MacBook-Pro-2.local>
2020-04-12 22:31:54 -0600

    updated terraform version

--- 02_GlobalVPCNetwork/terraform_install_amazon_linux2.sh
+++ 02_GlobalVPCNetwork/terraform_install_amazon_linux2.sh
@@ -2,9 +2,9 @@
 #Quick commands to get Terraform installed on Amazon Linux 2
 #Should work on other Linux releases but this is the only version I tested
 #Chad Smith - 11/06/2019
-wget https://releases.hashicorp.com/terraform/0.12.13/terraform_0.12.13_linux_amd64.zip
-unzip terraform_0.12.13_linux_amd64.zip
-sudo mv terraform /usr/local/bin/ && rm terraform_0.12.13_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip
+unzip terraform_0.12.24_linux_amd64.zip
+sudo mv terraform /usr/local/bin/ && rm terraform_0.12.24_linux_amd64.zip
 sudo ln -s /usr/local/bin/terraform /usr/bin/
 terraform --version
 
